0470daa021077f53de97bcc8a3fc03c84f714ebd
Adebowale <adebowaletemitayo40@gmail.com>
2023-02-19 23:59:01 +0100

    Updating firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,13 +3,12 @@ import "firebase/compat/firestore";
 import "firebase/compat/auth";
 
 const config = {
-  apiKey: "AIzaSyA2kSuT8PLVaIWqe_VTDdgyzDEYxRq5sDE",
-  authDomain: "crown-db-69d6a.firebaseapp.com",
-  projectId: "crown-db-69d6a",
-  storageBucket: "crown-db-69d6a.appspot.com",
-  messagingSenderId: "719238258146",
-  appId: "1:719238258146:web:4fd65c649880d15b812dbb",
-  measurementId: "G-1DFFTRL9YN",
+  apiKey: "AIzaSyDjy4zLjuef1uSQh24bkilVJjNNlj1xcG8",
+  authDomain: "crwn-ltd.firebaseapp.com",
+  projectId: "crwn-ltd",
+  storageBucket: "crwn-ltd.appspot.com",
+  messagingSenderId: "214325879315",
+  appId: "1:214325879315:web:da858e581ad346a70715e6",
 };
 
 export const creatUserprofileDocument = async (userAuth, additionalData) => {
@@ -49,3 +48,13 @@ provider.setCustomParameters({ prompt: "select_account" });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
+
+// {
+//   apiKey: "AIzaSyA2kSuT8PLVaIWqe_VTDdgyzDEYxRq5sDE",
+//   authDomain: "crown-db-69d6a.firebaseapp.com",
+//   projectId: "crown-db-69d6a",
+//   storageBucket: "crown-db-69d6a.appspot.com",
+//   messagingSenderId: "719238258146",
+//   appId: "1:719238258146:web:4fd65c649880d15b812dbb",
+//   measurementId: "G-1DFFTRL9YN",
+// };
